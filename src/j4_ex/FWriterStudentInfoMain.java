4bb4b21aee302e4e0a47ecea9756aaa2240b66e3
transit <transit@192.168.10.102>
2026-01-22 11:24:48 +0900

    fix4

--- src/j4_ex/FWriterStudentInfoMain.java
+++ src/j4_ex/FWriterStudentInfoMain.java
@@ -5,8 +5,6 @@ public class FWriterStudentInfoMain {
 	public static void main(String[] args) {
 		
 		FwriterStudentListBL bl = new FwriterStudentListBL();
-		System.out.println("#--受講生一覧--");
-		System.out.println("名前, 性別, 年齢, 職歴, 登録支店, 受講講座");
 		bl.extractAll();
 
 	}
--- src/j4_ex/FwriterStudentListBL.java
+++ src/j4_ex/FwriterStudentListBL.java
@@ -1,10 +1,16 @@
 package j4_ex;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class FwriterStudentListBL {
 
-	private final String COMMA = ",";
+	//定数の定義
+	final String FILE_PATH = "D:\\Javaデータベース活用編\\J4-EX_総合演習（JDBC）\\j4-EX_1.csv"; //書き出し用ファイルのパス情報
+	final String NEW_LINE = System.getProperty("line.separator"); //改行
+	final String COMMA = ","; // コンマ
 
 	public void extractAll() {
 
@@ -12,25 +18,39 @@ public class FwriterStudentListBL {
 		StudentListDao dao = new StudentListDao();
 		List<StudentListDto> dtoList = dao.selectMemberInfoAll();
 
-		if (dtoList != null) {
-			for (int i = 0; i < dtoList.size(); i++) {
-				StringBuffer rsbuf = new StringBuffer();
-				rsbuf.append(dtoList.get(i).getName());
-				rsbuf.append(COMMA);
-				rsbuf.append(dtoList.get(i).getGender());
-				rsbuf.append(COMMA);
-				rsbuf.append(dtoList.get(i).getAge());
-				rsbuf.append(COMMA);
-				rsbuf.append(dtoList.get(i).getCareer_mon());
-				rsbuf.append(COMMA);
-				rsbuf.append(dtoList.get(i).getBranch());
-				rsbuf.append(COMMA);
-				rsbuf.append(dtoList.get(i).getCourse());
+		try {
+			//書き出し用ファイルの読み込み
+	    	File file = new File( FILE_PATH );
 
-				System.out.println(rsbuf.toString());
-			}
-		} else {
-			System.out.println("[INFO]該当のユーザー情報を取得できませんでした");
+	        //書き出し用ファイルをFileWriterにセット
+	        FileWriter fileWriter = new FileWriter(file);
+	        
+	        fileWriter.write("#--受講生一覧--");
+	        fileWriter.write(NEW_LINE);
+	        fileWriter.write("名前, 性別, 年齢, 職歴, 登録支店, 受講講座");
+	        fileWriter.write(NEW_LINE);
+	        
+	        for (int i = 0; i < dtoList.size(); i++) {
+	        	fileWriter.write(dtoList.get(i).getName());
+	        	fileWriter.write( COMMA );
+	        	fileWriter.write(dtoList.get(i).getGender());
+	        	fileWriter.write( COMMA );
+	        	fileWriter.write(String.valueOf(dtoList.get(i).getAge()));
+	        	fileWriter.write( COMMA );
+	        	fileWriter.write(dtoList.get(i).getCareer_mon());
+	        	fileWriter.write( COMMA );
+	        	fileWriter.write(dtoList.get(i).getBranch());
+	        	fileWriter.write( COMMA );
+	        	fileWriter.write(dtoList.get(i).getCourse());
+	        	fileWriter.write(NEW_LINE);
+	        }
+	        
+	        fileWriter.write(NEW_LINE);
+	        
+	        fileWriter.close();
+	        
+		} catch (IOException e) {
+			System.out.println(e);
 		}
 
 	}
